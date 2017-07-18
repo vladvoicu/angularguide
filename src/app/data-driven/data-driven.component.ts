f74c900a257e267629b71e072616128fd713be7c
Voicu Vlad <Voicu Vlad>
2017-07-18 15:27:49 +0300

    forms done

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl,Validators } from "@angular/forms";
+import { FormGroup,FormControl,Validators,FormArray,FormBuilder } from "@angular/forms";
+import {Observable} from "rxjs/Rx";
 
 @Component({
   selector: 'app-data-driven',
@@ -8,17 +9,45 @@ import { FormGroup,FormControl,Validators } from "@angular/forms";
 })
 export class DataDrivenComponent  {
 myForm:FormGroup;
-constructor(){
+genders=['male','female'];
+constructor(private formBuilder:FormBuilder){
   this.myForm=new FormGroup({
     'userData':new FormGroup({
-       'username':new FormControl('Vlad',Validators.required),
+       'username':new FormControl('Vlad',[Validators.required,this.exampleValidator]),
     'email':new FormControl('',[Validators.required,Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])
     }),
     
-    'password':new FormControl('',Validators.required)
+    'password':new FormControl('',Validators.required),
+    'gender':new FormControl('male'),
+    'hobbies':new FormArray([new FormControl('cooking',Validators.required,this.asyncExample)])
   });
+  this.myForm.valueChanges.subscribe(
+    (data:any)=>console.log(data)
+  );
+  
 }
   onSubmit(){
     console.log(this.myForm);
+  }
+  addHobby(){
+    (<FormArray>this.myForm.controls['hobbies']).push(new FormControl('',Validators.required,this.asyncExample));
+  }
+  exampleValidator(control:FormControl):{[s:string]:boolean}{
+    if(control.value==='Example'){
+      return {example:true};
+    }
+    return null;
+  }
+  asyncExample(control:FormControl):Promise<any>|Observable<any>{
+    const promise=new Promise<any>(
+      (resolve,reject)=>{
+        setTimeout(()=>{
+          if(control.value==='Example')
+          {resolve({'invalid':true});}
+          else{resolve(null);}
+        },1500);
+      }
+    );
+    return promise;
   }
 }
